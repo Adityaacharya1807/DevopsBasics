d31fa0ed4557a885cc93d102fd3d117189a2e972
Adityaacharya1807 <aditya.acharya@moolya.com>
2020-09-09 11:26:23 +0530

    Hook Test

---
+++ server/src/test/java/com/example/test_hook.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class test_hook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO GIT-HOOK TEST");
+
+	}
+
+}
